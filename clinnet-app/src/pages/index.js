d11fdbcee5229daebe156ef883a3a93e28bfafef
Senglong Ngor <senglong.ngor@gmail.com>
2025-05-06 16:45:37 -0700

    update index js

--- clinnet-app/src/pages/index.js
+++ clinnet-app/src/pages/index.js
@@ -1,19 +1,19 @@
 // src/pages/index.js
 // This file exports all pages to make imports cleaner throughout the application
 
-export { default as LoginPage } from './LoginPage';
-export { default as AdminDashboard } from './AdminDashboard';
-export { default as DoctorDashboard } from './DoctorDashboard';
-export { default as FrontdeskDashboard } from './FrontdeskDashboard';
-export { default as PatientManagementPage } from './PatientManagementPage';
-export { default as AdminPatientsPage } from './AdminPatientsPage';
-export { default as AdminServicesPage } from './AdminServicesPage';
-export { default as PatientDetailPage } from './PatientDetailPage';
-export { default as NewPatientPage } from './NewPatientPage';
-export { default as NotFoundPage } from './NotFoundPage';
-export { default as UserManagementPage } from './UserManagementPage';
-export { default as AccountSettingsPage } from './AccountSettingsPage';
-export { default as FrontdeskAppointmentsPage } from './FrontdeskAppointmentsPage';
-export { default as DoctorAppointmentsPage } from './DoctorAppointmentsPage';
-export { default as AdminAppointmentsPage } from './AdminAppointmentsPage';
-export { default as StyleGuidePage } from './StyleGuidePage';
+export { default as LoginPage } from './LoginPage.jsx';
+export { default as AdminDashboard } from './AdminDashboard.jsx';
+export { default as DoctorDashboard } from './DoctorDashboard.jsx';
+export { default as FrontdeskDashboard } from './FrontdeskDashboard.jsx';
+export { default as PatientManagementPage } from './PatientManagementPage.jsx';
+export { default as AdminPatientsPage } from './AdminPatientsPage.jsx';
+export { default as AdminServicesPage } from './AdminServicesPage.jsx';
+export { default as PatientDetailPage } from './PatientDetailPage.jsx';
+export { default as NewPatientPage } from './NewPatientPage.jsx';
+export { default as NotFoundPage } from './NotFoundPage.jsx';
+export { default as UserManagementPage } from './UserManagementPage.jsx';
+export { default as AccountSettingsPage } from './AccountSettingsPage.jsx';
+export { default as FrontdeskAppointmentsPage } from './FrontdeskAppointmentsPage.jsx';
+export { default as DoctorAppointmentsPage } from './DoctorAppointmentsPage.jsx';
+export { default as AdminAppointmentsPage } from './AdminAppointmentsPage.jsx';
+export { default as StyleGuidePage } from './StyleGuidePage.jsx';
