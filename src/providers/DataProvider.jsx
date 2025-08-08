a81942fb5161c35183a9de2b399e3cef957d07dc
Senglong Ngor <senglong.ngor@gmail.com>
2025-08-07 22:07:24 -0700

    Remove unused files and update API base URLs for local development

--- src/providers/DataProvider.jsx
+++
@@ -1,7 +0,0 @@
-// Look for API calls related to services and update them:
-
-// Change URLs from:
-// https://libzdu3ibi.execute-api.us-east-2.amazonaws.com/prod/services
-
-// To:
-// http://localhost:3001/api/services
--- src/services/apiService.js
+++
@@ -1,5 +0,0 @@
-// Find this line or similar:
-const API_BASE_URL = 'https://libzdu3ibi.execute-api.us-east-2.amazonaws.com/prod';
-
-// Change it to:
-const API_BASE_URL = 'http://localhost:3001/api';
